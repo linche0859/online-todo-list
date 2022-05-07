ef60bf12711ccc6e1ff86f9eb9ed1d34f9e275bc
che.lin <aa10327tw@gmail.com>
2022-05-07 23:04:02 +0800

    feat: 首頁切版

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,31 @@ module.exports = {
       'noto-sans': ['"Noto Sans TC"', 'sans-serif'],
     },
     borderRadius: {
-      large: '10px',
+      sm: '5px',
+      lg: '10px',
     },
     extend: {
       colors: {
         primary: '#ffd370',
         gray: {
           100: '#333333',
+          200: '#9F9A91',
+          300: '#E5E5E5',
+          400: '#EFEFEF',
         },
         red: {
           100: '#D87355',
         },
+        yellow: {
+          100: '#FFD370',
+        },
+      },
+      backgroundImage: {
+        index:
+          'linear-gradient(172.7deg, #FFD370 5.12%, #FFD370 53.33%, #FFD370 53.44%, #FFFFFF 53.45%, #FFFFFF 94.32%)',
+      },
+      boxShadow: {
+        index: '0px 0px 15px rgba(0, 0, 0, 0.15)',
       },
     },
   },
